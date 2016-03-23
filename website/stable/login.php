625d74e5ef8127b4cf2a433bcb16d51176daea13
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-03-22 18:49:29 -0600

    Fixed login issue

--- website/stable/login.php
+++ website/stable/login.php
@@ -1,5 +1,9 @@
 <?php
 include('pageContent.php');
+
+// TODO Better division of permissions
+// Can't use dbReader because things need to be inserted into sessions
+$dbm = new DBManager('arphen', 'logindb');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
